b7a3b0e5b63bffaeb88bd78569cae97a9d5f3ea1
Anatolii Fenin <fenintolja@gmail.com>
2025-01-15 10:33:17 +0200

    bounded

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,3 +1,4 @@
+import { Bounded } from '@/components/Bounded';
 import { Content } from '@prismicio/client';
 import { PrismicNextLink } from '@prismicio/next';
 import { PrismicRichText, SliceComponentProps } from '@prismicio/react';
@@ -13,7 +14,7 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className="text-red-600"
@@ -21,7 +22,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       <PrismicRichText field={slice.primary.heading} />
       <PrismicRichText field={slice.primary.body} />
       <PrismicNextLink field={slice.primary.button} />
-    </section>
+    </Bounded>
   );
 };
 
