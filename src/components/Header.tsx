af5c1d9d82ca0f69ef119d937e33c0410bea9d5e
Anatolii Fenin <fenintolja@gmail.com>
2025-01-20 10:10:51 +0200

    product grid

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
 import Link from 'next/link';
 import { ButtonLink } from './ButtonLink';
 import { Logo } from './Logo';
+import { createClient } from '@/prismicio';
+import { PrismicNextLink } from '@prismicio/next';
 
-export const Header = () => {
+export const Header = async () => {
+  const client = createClient();
+  const settings = await client.getSingle('settings');
   return (
     <header className="header absolute top-0 left-0 right-0 z-10 ~h-32/48 ~px-4/6 ~py-4/6 md:h-32">
       <div className="mx-auto grid w-full max-w-6xl grid-cols-[auto,auto] items-center gap-6 md:grid-cols-[1fr,auto,1fr] ">
@@ -14,9 +18,11 @@ export const Header = () => {
           className="col-span-full row-start-2 md:col-span-1 md:col-start-2 md:row-start-1"
         >
           <ul className="flex flex-wrap items-center justify-center gap-8">
-            <li>Boards</li>
-            <li>Boards</li>
-            <li>Boards</li>
+            {settings.data.navigation.map((item) => (
+              <li key={item.link.text}>
+                <PrismicNextLink field={item.link} className="~text-lg/xl" />
+              </li>
+            ))}
           </ul>
         </nav>
         <div className="justify-self-end">
---
+++ src/slices/ProductGrid/index.tsx
@@ -0,0 +1,40 @@
+import { Bounded } from '@/components/Bounded';
+import { Heading } from '@/components/Heading';
+import { Content, isFilled } from '@prismicio/client';
+import {
+  PrismicRichText,
+  PrismicText,
+  SliceComponentProps,
+} from '@prismicio/react';
+import { JSX } from 'react';
+import Skateboard from './Skateboard';
+
+export type ProductGridProps = SliceComponentProps<Content.ProductGridSlice>;
+
+const ProductGrid = ({ slice }: ProductGridProps): JSX.Element => {
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="bg-texture bg-brand-gray"
+    >
+      <Heading as="h2" className="text-center ~mb-4/6">
+        <PrismicText field={slice.primary.heading} />
+      </Heading>
+      <div className="text-center ~mb-6-10">
+        <PrismicRichText field={slice.primary.body} />
+      </div>
+
+      <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+        {slice.primary.product.map(
+          ({ skateboard }) =>
+            isFilled.contentRelationship(skateboard) && (
+              <Skateboard key={skateboard.id} id={skateboard.id} />
+            )
+        )}
+      </div>
+    </Bounded>
+  );
+};
+
+export default ProductGrid;
